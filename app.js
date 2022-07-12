cdc78c344f3fc6ef6c301a1db37a8a480ba528a5
oyeProjects <oyeprojects@hotmail.com>
2022-07-12 17:23:11 +0300

    Workspace created

---
+++ app.js
@@ -0,0 +1,20 @@
+// Prettier'i kullanmak için şunları yaptık : 
+//-> Extension'u indirdik
+//-> Sonra 'CTRL + SHIFT + P' ile prettier'i seçtik ve dosyamızı 'select'ledik
+//-> Daha sonra prettier'in dokumantasyonundaki 'npm install'i yaptık
+//-> Bazı ek isteklerimizi '.prettierrc'e ekledik. Örneğin : semi : true --> noktali virgul eklemesi icin  "singleQuote": false -> tek tırnak kullanması icin
+// -> Böylelikle her 'SHIFT + ALT+F' yaptıgımızda documantasyon düzelecektir.
+
+// Bazı dosyaların 'repository'de olmasını istemeyiz , onların değişikliğini takip etmek istemeyiz. Bunun için 'gitignore' kullanmamız gerekir.
+// Google'a 'gitignore' yazıyoruz ve karşımıza 'toptal'ın bir sitesi çıkıyor.
+// 'node' yazarak nodeJs için oluşuturulan '.gitignore' dosyasını indiyoruz.
+
+// Sayfamızı github'a pushlamamız gerekir. Bunun için :
+// -> git status (status durumuna göz atmamız için)
+
+
+
+
+const fn = function () {
+    console.log("Function working");
+}
